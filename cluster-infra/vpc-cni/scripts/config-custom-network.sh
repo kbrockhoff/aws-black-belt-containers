a019d99b71f66d45777cebad5dae67a269ebac61
Kevin Brockhoff <kbrockhoff@codekaizen.org>
2022-06-25 14:05:51 -0500

    add instance restart, http redirect, launch template

--- cluster-infra/vpc-cni/scripts/config-custom-network.sh
+++ cluster-infra/vpc-cni/scripts/config-custom-network.sh
@@ -8,5 +8,6 @@ CUR_VALUE=`kubectl describe ds aws-node --server=$KUBESERVER --token=$KUBETOKEN 
 if [[ $CUR_VALUE == *"false"* ]]; then
   echo "Setting custom configuration to true"
   kubectl set env ds aws-node --server=$KUBESERVER --token=$KUBETOKEN --certificate-authority=$KUBECA -n kube-system AWS_VPC_K8S_CNI_CUSTOM_NETWORK_CFG=true ENABLE_PREFIX_DELEGATION=true WARM_PREFIX_TARGET=1 ENI_CONFIG_LABEL_DEF=failure-domain.beta.kubernetes.io/zone
-
+  INSTANCES=`aws ec2 describe-instances --filters Name=instance-state-name,Values=running Name=tag:kubernetes.io/cluster/$CLUSTERNAME,Values=owned --query "Reservations[].Instances[].InstanceID" --output text`
+  aws ec2 terminate-instances --no-cli-pager --instance-ids $INSTANCES
 fi
