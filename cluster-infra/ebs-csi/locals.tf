331bf32c1a6d42ecaa6beaefd463ce59f347fcae
Kevin Brockhoff <kbrockhoff@codekaizen.org>
2022-06-28 12:20:26 -0500

    remove obsolete local

--- cluster-infra/ebs-csi/locals.tf
+++ cluster-infra/ebs-csi/locals.tf
@@ -1,9 +1,3 @@
 locals {
   initial_storage_class_name = data.kubernetes_storage_class_v1.default.metadata[0].name
-
-  kubeconfig_options = length(var.kubeconfig_file) > 0 && length(var.kubeconfig_name) > 0 ? (
-    "--kubeconfig=${var.kubeconfig_file} --context=${var.kubeconfig_name}"
-    ) : (
-    ""
-  )
 }
