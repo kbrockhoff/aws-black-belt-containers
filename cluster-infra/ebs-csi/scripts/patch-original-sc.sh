596d6499290c14639c621bbc7f35524afaced36b
Kevin Brockhoff <kbrockhoff@codekaizen.org>
2022-06-29 11:22:30 -0500

    fix quotes

--- cluster-infra/ebs-csi/scripts/patch-original-sc.sh
+++ cluster-infra/ebs-csi/scripts/patch-original-sc.sh
@@ -5,4 +5,4 @@ set -e
 echo "Calling $KUBESERVER ..."
 kubectl --server="$KUBESERVER" --token="$KUBETOKEN" --certificate-authority="$KUBECA" \
   patch storageclass "$SC" \
-  -p '{\"metadata\": {\"annotations\":{\"storageclass.kubernetes.io/is-default-class\":\"false\"}}}'
+  -p "{\"metadata\": {\"annotations\":{\"storageclass.kubernetes.io/is-default-class\":\"false\"}}}"
