8aca952f2e5af11cb90f0c8a90442f96e8b8b79b
Kevin Brockhoff <kbrockhoff@codekaizen.org>
2022-06-29 08:18:42 -0500

    fix external result

--- cluster-infra/vpc-cni/locals.tf
+++ cluster-infra/vpc-cni/locals.tf
@@ -1,5 +1,5 @@
 locals {
   cni_patch_needed = var.enabled && var.enable_custom_network ? (
-    data.external.cni_cfg[0].customnetwork == "false"
+    data.external.cni_cfg[0].result["customnetwork"] == "false"
   ) : false
 }
