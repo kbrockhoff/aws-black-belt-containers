a4e48d401b41f898319763d8d0784092df59b67a
Kevin Brockhoff <kbrockhoff@codekaizen.org>
2022-06-30 13:53:09 -0500

    change cert-manager iam role to match blueprints

--- cluster-infra/cert-manager/iam.tf
+++ cluster-infra/cert-manager/iam.tf
@@ -1,34 +1,37 @@
-data "aws_iam_policy_document" "acme_issuer_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-    effect  = "Allow"
+data "aws_iam_policy_document" "certmgr_assume_role" {
+  count = var.enabled ? 1 : 0
 
+  statement {
+    effect = "Allow"
+    principals {
+      identifiers = [var.oidc_provider_arn]
+      type        = "Federated"
+    }
+    actions = ["sts:AssumeRoleWithWebIdentity"]
     condition {
       test     = "StringEquals"
       variable = "${replace(var.cluster_oidc_issuer_url, "https://", "")}:sub"
       values   = ["system:serviceaccount:${var.namespace}:${local.name_certmgr}"]
     }
-
-    principals {
-      identifiers = [var.oidc_provider_arn]
-      type        = "Federated"
-    }
   }
 }
 
-resource "aws_iam_role" "acme_issuer" {
-  count = local.enabled_dns01 ? 1 : 0
+resource "aws_iam_role" "certmgr" {
+  count = var.enabled ? 1 : 0
 
-  assume_role_policy = data.aws_iam_policy_document.acme_issuer_assume_role_policy.json
-  name               = "${local.name_certmgr}-sa"
+  assume_role_policy = data.aws_iam_policy_document.certmgr_assume_role[0].json
+  name               = local.name_iam
 
   tags = merge(var.tags, {
-    Name = "${local.name_certmgr}-sa"
+    Name = local.name_iam
   })
 }
 
 data "aws_iam_policy_document" "route53" {
+  count = var.enabled ? 1 : 0
+
   statement {
+    sid    = "Route53Updates"
     effect = "Allow"
     actions = [
       "route53:GetChange",
@@ -38,6 +41,7 @@ data "aws_iam_policy_document" "route53" {
     ]
   }
   statement {
+    sid    = "Route53Updates"
     effect = "Allow"
     actions = [
       "route53:ChangeResourceRecordSets",
@@ -48,6 +52,7 @@ data "aws_iam_policy_document" "route53" {
     ]
   }
   statement {
+    sid    = "Route53Reads"
     effect = "Allow"
     actions = [
       "route53:ListHostedZonesByName",
@@ -59,15 +64,15 @@ data "aws_iam_policy_document" "route53" {
 }
 
 resource "aws_iam_policy" "route53" {
-  count = local.enabled_dns01 ? 1 : 0
+  count = var.enabled ? 1 : 0
 
-  name   = "${local.name_certmgr}-route53-policy"
-  policy = data.aws_iam_policy_document.route53.json
+  name   = local.name_iam
+  policy = data.aws_iam_policy_document.route53[0].json
 }
 
 resource "aws_iam_role_policy_attachment" "route53" {
-  count = local.enabled_dns01 ? 1 : 0
+  count = var.enabled ? 1 : 0
 
   policy_arn = aws_iam_policy.route53[0].arn
-  role       = aws_iam_role.acme_issuer[0].name
+  role       = aws_iam_role.certmgr[0].name
 }
