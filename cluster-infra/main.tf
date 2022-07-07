9c6967e5aef59cd2bd52bd7c4b6bdc8ebe70b41a
Kevin Brockhoff <kbrockhoff@codekaizen.org>
2022-07-07 16:41:45 -0500

    extract single value from list

--- cluster-infra/main.tf
+++ cluster-infra/main.tf
@@ -337,7 +337,7 @@ module "karpenter_provisioning" {
   karpenter_provisioner_name       = "default"
   eks_cluster_version              = module.eks_blueprints.eks_cluster_version
   worker_node_security_group_id    = module.eks_blueprints.worker_node_security_group_id
-  worker_node_iam_instance_profile = module.eks_blueprints.managed_node_group_iam_instance_profile_id
+  worker_node_iam_instance_profile = module.eks_blueprints.managed_node_group_iam_instance_profile_id[0]
   launch_template_pre_userdata     = templatefile("${path.module}/templates/eks-nodes-userdata.sh", {})
 
   depends_on = [module.eks_blueprints_base_addons]
