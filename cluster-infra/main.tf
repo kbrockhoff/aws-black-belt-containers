847d94a2c76e0d31e4ae0338602007dfac250ea4
Kevin Brockhoff <kbrockhoff@codekaizen.org>
2022-07-11 16:27:53 -0500

    fix template input

--- cluster-infra/main.tf
+++ cluster-infra/main.tf
@@ -309,6 +309,7 @@ module "prometheus_stack" {
     values = [templatefile("${path.module}/templates/kube-prom-stack-values.yaml", {
       alertmanager_hosts = local.alertmanager_hosts
       grafana_hosts      = local.grafana_hosts
+      initial_passwd     = random_password.grafana.result
     })]
   }
   addon_context = {
@@ -321,7 +322,6 @@ module "prometheus_stack" {
     eks_oidc_issuer_url            = module.eks_blueprints.eks_oidc_issuer_url
     eks_oidc_provider_arn          = module.eks_blueprints.eks_oidc_provider_arn
     tags                           = module.this.tags
-    initial_passwd                 = random_password.grafana.result
   }
   ingress_hostnames = concat(local.alertmanager_hosts, local.grafana_hosts)
   route53_zone_id   = data.aws_route53_zone.public.zone_id
