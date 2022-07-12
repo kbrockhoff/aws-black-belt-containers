f545b43016e307385ee3174b977b5bd9b4ecc3d0
Kevin Brockhoff <kbrockhoff@codekaizen.org>
2022-07-12 09:02:24 -0500

    fix provisioner def

--- cluster-infra/karpenter-provision/main.tf
+++ cluster-infra/karpenter-provision/main.tf
@@ -60,7 +60,6 @@ spec:
       cpu: 1000
       memory: 1000Gi
   provider:
-    instanceProfile: ${var.worker_node_iam_instance_profile}
     launchTemplate: '${module.karpenter_launch_template.launch_template_name["linux"]}'
     subnetSelector:
       dbs-networktags: private
